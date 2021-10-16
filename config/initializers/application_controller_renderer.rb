41bb3598fd69d4ac821ab6570b4cb8e8ab37e66b
kitayama3352 <kitayama3352@gmail.com>
2021-10-16 12:56:04 +0000

    3章【画像投稿機能を作ろう】 END

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '49e9e71f58bd2748b39113ce7df601b9f8a7f0ae74d054b1a2c3f5eb946a8631bf9fb31609c6833c8f296ca9b9d96139b4d542311a0514e122ed5e4ab46b7a1f'
